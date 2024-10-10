9a739b9f4a173045be759aadb2d71384f1f81f49
Molduin <molduin@proton.me>
2024-10-10 15:29:49 +0200

    Fetching does not function locally

--- Darknet/naïveCrypt.js
+++ Darknet/naïveCrypt.js
@@ -6,6 +6,8 @@
 
 // import {jsSHA} from "./sha512";
 
+fetchFile("Files/index.txt");
+
 const typeSizes = {
     "undefined": () => 0,
     "boolean": () => 4,
@@ -21,9 +23,10 @@ const sizeOf = value => typeSizes[typeof value](value);
 const globalSalt = [177, 211, 123, 249, 2, 30, 164, 89, 87, 96, 24, 156, 216, 182, 206, 199];
 
 let ciphertext = encrypt("Hello, world!","hunter2");
-console.log(new TextDecoder().decode(ciphertext)); // Should be some byte salad
+console.log(ciphertext); // Should be some byte salad
 let decrypted = encrypt(ciphertext, "hunter2");
 console.log(new TextDecoder().decode(decrypted)); // Should say "Hello, world!"
+
 //console.log("Hello");
 
 /**
@@ -55,7 +58,7 @@ function encrypt(text, key) {
         }
 
         function generateBlock(lastBlock, key) {
-            return (new jsSHA("SHA-512", "UINT8ARRAY").update([...currentBlock, ...key]).getHash("UINT8ARRAY"));
+            return (new jsSHA("SHA-512", "UINT8ARRAY").update([...lastBlock, ...key]).getHash("UINT8ARRAY"));
         }
 
         return allBlocks;
@@ -78,10 +81,31 @@ function encrypt(text, key) {
     }
 }
 
-function decrypt(data, key) {
-
+function multiHash(input, iterationArray, iterations) {
+    let output = input;
+    for(let i = 0; i < iterations; i++){
+        for (let j = 0; j < iterationArray.length; j++) {
+            let concatenate = false;
+            for (let k = 0; k < iterationArray[j]; k++) {
+                let shaObj = new jsSHA("SHA-512", "UINT8ARRAY");
+                if (concatenate) output = [...output, ...input];
+                output = shaObj.update(output).getHash("UINT8ARRAY");
+            }
+            concatenate = !concatenate;
+        }
+    }
+    return output;
 }
 
-function findFile(key) {
+async function fetchFile(filename) {
+    try {
+        const response = await fetch(filename);
 
+        if (!response.ok) throw new Error('Failed to fetch document');
+        let data = await response.text();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
 }
